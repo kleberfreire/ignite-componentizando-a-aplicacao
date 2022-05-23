136a6a4a46d887fd51bd54f2b40382e18ad1fc27
Kleber Freire <ch4r4d4@gmail.com>
2022-05-23 17:40:21 -0300

    desafio pronto

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+interface ISidebarProps {
+  selectedGenreId: GenreResponseProps["id"];
+  genres: GenreResponseProps[];
+  handleClickButton: (genre_id: number) => void;
+}
+
+export function SideBar({
+  selectedGenreId,
+  genres,
+  handleClickButton,
+}: ISidebarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
